584f8cb6fc2a3985a29287ac46c676ac6335c62a
KillTheMule <KillTheMule@users.noreply.github.com>
2017-11-28 21:40:31 +0100

    Add benchmarks

---
+++ benches/card.rs
@@ -0,0 +1,25 @@
+#![feature(test)]
+extern crate test;
+extern crate nvimpam_lib;
+
+use self::test::Bencher;
+
+use nvimpam_lib::cards::Card;
+
+#[bench]
+fn bench_parse2folddata(b: &mut Bencher) {
+  use std::fs::File;
+  use std::io::{self, BufRead};
+
+  let file = File::open("files/example.pc").unwrap();
+  let v: Vec<String> = io::BufReader::new(file)
+    .lines()
+    .map(|l| l.unwrap())
+    .collect();
+
+  b.iter(|| {
+    let r = test::black_box(&v);
+    let cd = Card::create_card_data(r);
+    let compacted = Card::contract_card_data(&cd);
+  })
+}
