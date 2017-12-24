485a474f704e7fdfce7d36becbb9257636184a5f
KillTheMule <KillTheMule@users.noreply.github.com>
2017-12-24 01:02:37 +0100

    Update benchmark

--- benches/card.rs
+++ benches/card.rs
@@ -7,27 +7,9 @@ use self::test::Bencher;
 use nvimpam_lib::card::keyword::Keyword;
 use nvimpam_lib::folds::FoldList;
 
+
 #[bench]
 fn bench_parse2folddata(b: &mut Bencher) {
-  use std::fs::File;
-  use std::io::{self, BufRead};
-
-  let file = File::open("files/example.pc").unwrap();
-  let v: Vec<String> = io::BufReader::new(file)
-    .lines()
-    .map(|l| l.unwrap())
-    .collect();
-
-  let mut f = FoldList::new();
-  b.iter(|| {
-    let r = test::black_box(&v);
-    f.clear();
-    let _compacted = f.add_keyword_data(r);
-  })
-}
-
-#[bench]
-fn bench_parse2folddata_new(b: &mut Bencher) {
   use std::fs::File;
   use std::io::{self, BufRead};
 
