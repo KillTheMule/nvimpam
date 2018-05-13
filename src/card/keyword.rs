03ada03a56b56a74a42a2864aca95b43e2fb1eaa
KillTheMule <KillTheMule@users.noreply.github.com>
2018-05-13 22:11:54 +0200

    Make parse_str slower

--- src/card/keyword.rs
+++ src/card/keyword.rs
@@ -81,49 +81,49 @@ impl Keyword {
     let s = s.as_ref().as_bytes();
     let len = s.len();
 
-    if len == 0 || len < 8 {
+    if len == 0 || len < 7 {
       None
     } else {
-      let start = &s[0..8];
+      let start = &s[0..7];
 
       match start {
         // Node
-        b"NODE  / " => Some(Node),
-        b"CNODE / " => Some(Cnode),
-        b"MASS  / " => Some(Mass),
-        b"NSMAS / " => Some(Nsmas),
-        b"NSMAS2/ " => Some(Nsmas2),
-        // Element
-        b"SOLID / " => Some(Solid),
-        b"HEXA20/ " => Some(Hexa20),
-        b"PENT15/ " => Some(Pent15),
-        b"PENTA6/ " => Some(Penta6),
-        b"TETR10/ " => Some(Tetr10),
-        b"TETR4 / " => Some(Tetr4),
-        b"BSHEL / " => Some(Bshel),
-        b"TSHEL / " => Some(Tshel),
-        b"SHELL / " => Some(Shell),
-        b"SHEL6 / " => Some(Shel6),
-        b"SHEL8 / " => Some(Shel8),
-        b"MEMBR / " => Some(Membr),
-        b"BEAM  / " => Some(Beam),
-        b"SPRGBM/ " => Some(Sprgbm),
-        b"BAR   / " => Some(Bar),
-        b"SPRING/ " => Some(Spring),
-        b"JOINT / " => Some(Joint),
-        b"KJOIN / " => Some(Kjoin),
-        b"MTOJNT/ " => Some(Mtojnt),
-        b"SPHEL / " => Some(Sphel),
-        b"SPHELO/ " => Some(Sphelo),
-        b"GAP   / " => Some(Gap),
-        b"IMPMA / " => Some(Impma),
+        b"NODE  /" => Some(Node),
+        b"CNODE /" => Some(Cnode),
+        b"MASS  /" => Some(Mass),
+        b"NSMAS /" => Some(Nsmas),
+        b"NSMAS2/" => Some(Nsmas2),
+        // Elemen
+        b"SOLID /" => Some(Solid),
+        b"HEXA20/" => Some(Hexa20),
+        b"PENT15/" => Some(Pent15),
+        b"PENTA6/" => Some(Penta6),
+        b"TETR10/" => Some(Tetr10),
+        b"TETR4 /" => Some(Tetr4),
+        b"BSHEL /" => Some(Bshel),
+        b"TSHEL /" => Some(Tshel),
+        b"SHELL /" => Some(Shell),
+        b"SHEL6 /" => Some(Shel6),
+        b"SHEL8 /" => Some(Shel8),
+        b"MEMBR /" => Some(Membr),
+        b"BEAM  /" => Some(Beam),
+        b"SPRGBM/" => Some(Sprgbm),
+        b"BAR   /" => Some(Bar),
+        b"SPRING/" => Some(Spring),
+        b"JOINT /" => Some(Joint),
+        b"KJOIN /" => Some(Kjoin),
+        b"MTOJNT/" => Some(Mtojnt),
+        b"SPHEL /" => Some(Sphel),
+        b"SPHELO/" => Some(Sphelo),
+        b"GAP   /" => Some(Gap),
+        b"IMPMA /" => Some(Impma),
         // Link
-        b"ELINK / " => Some(Elink),
-        b"LLINK / " => Some(Llink),
-        b"SLINK / " => Some(Slink),
-        b"PLINK / " => Some(Plink),
-        b"TIED  / " => Some(Tied),
-        b"PART  / " => {
+        b"ELINK /" => Some(Elink),
+        b"LLINK /" => Some(Llink),
+        b"SLINK /" => Some(Slink),
+        b"PLINK /" => Some(Plink),
+        b"TIED  /" => Some(Tied),
+        b"PART  /" => {
           if len < 24 {
             None
           } else {
