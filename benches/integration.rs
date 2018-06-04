fe959f65a1f8cd02a81794a7b09692a9b176cb53
KillTheMule <KillTheMule@users.noreply.github.com>
2018-06-04 22:33:21 +0200

    Add the first real benchmark

---
+++ benches/integration.rs
@@ -0,0 +1,57 @@
+#![feature(test)]
+extern crate neovim_lib;
+extern crate nvimpam_lib;
+extern crate test;
+
+use std::process::Command;
+use std::sync::mpsc;
+
+use self::test::Bencher;
+
+use nvimpam_lib::event::Event::*;
+use nvimpam_lib::folds::FoldList;
+use nvimpam_lib::handler::NeovimHandler;
+use nvimpam_lib::lines::Lines;
+use nvimpam_lib::neovim_ext::BufferExt;
+
+use neovim_lib::neovim::Neovim;
+use neovim_lib::neovim_api::NeovimApi;
+use neovim_lib::session::Session;
+
+#[bench]
+fn bench_folds(b: &mut Bencher) {
+  let (sender, receiver) = mpsc::channel();
+  let mut session = Session::new_child_cmd(
+    Command::new("neovim/build/bin/nvim")
+      .args(&["-u", "NONE", "--embed"])
+      .env_clear()
+      .env("VIMRUNTIME", "neovim/runtime")
+  ).unwrap();
+
+  session.start_event_loop_handler(NeovimHandler(sender));
+  let mut nvim = Neovim::new(session);
+
+  nvim.command("e files/example.pc").expect("2");
+
+  let curbuf = nvim.get_current_buf().expect("3");
+
+  let mut foldlist = FoldList::new();
+  let mut lines = Lines::new(Vec::new());
+
+
+  b.iter(|| {
+    curbuf.event_sub(&mut nvim, true).expect("4");
+    loop {
+      match receiver.recv() {
+        Ok(UpdatesStart { linedata, .. }) => {
+          lines = Lines::new(linedata);
+          foldlist.recreate_all(&lines).expect("5");
+          foldlist.resend_all(&mut nvim).expect("6");
+          curbuf.event_unsub(&mut nvim).expect("7");
+          nvim.command("call rpcnotify(1, 'quit')").unwrap();
+        }
+        _ => break,
+      }
+    }
+  })
+}
