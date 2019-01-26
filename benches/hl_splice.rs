66dde95d892d84e2cc72bccba8c69c8894c9b2de
KillTheMule <KillTheMule@users.noreply.github.com>
2019-01-26 17:05:37 +0100

    More highlight benchmarks

--- benches/hl_splice.rs
+++ benches/hl_splice.rs
@@ -2,12 +2,95 @@ extern crate nvimpam_lib;
 
 #[macro_use]
 extern crate criterion;
+extern crate neovim_lib;
+
 use criterion::{black_box, Criterion};
+
+use neovim_lib::Value;
 
 use nvimpam_lib::{bufdata::BufData, card::keyword::Keywords, lines::Lines};
 
+fn fake_highlight_region(
+  bd: &BufData,
+  firstline: u64,
+  lastline: u64,
+) -> Vec<Value> {
+  let mut calls: Vec<Value> = vec![];
+
+  calls.push(
+    vec![
+      Value::from("nvim_buf_clear_highlight".to_string()),
+      vec![
+        Value::from(1),
+        Value::from(5),
+        Value::from(firstline),
+        Value::from(lastline),
+      ]
+      .into(),
+    ]
+    .into(),
+  );
+
+  for ((l, s, e), t) in bd.highlights.iter() {
+    if firstline <= *l && *l < lastline {
+      let st: &'static str = (*t).into();
+      calls.push(
+        vec![
+          Value::from("nvim_buf_add_highlight".to_string()),
+          vec![
+            Value::from(1),
+            Value::from(5),
+            Value::from(st.to_string()),
+            Value::from(*l),
+            Value::from(u64::from(*s)),
+            Value::from(u64::from(*e)),
+          ]
+          .into(),
+        ]
+        .into(),
+      );
+    } else if *l > lastline {
+      break;
+    }
+  }
+  calls
+}
+
+macro_rules! hl_bench {
+  ($fn: ident; lines: ($start: expr, $end: expr);
+   spliceto: ($sstart: expr, $ssend: expr, $added: expr)) => {
+    fn $fn(c: &mut Criterion) {
+      c.bench_function(stringify!($fn), move |b| {
+        let origlines = Lines::read_file("files/example.pc").expect("1");
+        let lines = Lines::from_slice(&origlines);
+        let keywords = Keywords::from_lines(&lines);
+        let mut bufdata = BufData::new();
+        bufdata.recreate_all(&keywords, &lines).expect("2");
+
+        // example.pc has 20586 lines, so 20587 is the last valid linenumber
+        // so 20586 is the last valid line index
+        assert!($end < 20587);
+        assert!($ssend < 20587);
+
+        b.iter(|| {
+          let mut tmp_bufdata = black_box(BufData::new());
+          tmp_bufdata
+            .recreate_all(&keywords[$start..$end], &lines[$start..$end])
+            .expect("3");
+          bufdata.splice(tmp_bufdata, $sstart, $ssend, $added);
+          let _calls = black_box(fake_highlight_region(
+            &bufdata,
+            $sstart,
+            (($ssend as i64) + $added) as u64,
+          ));
+        })
+      });
+    }
+  };
+}
+
 fn bench_bufdata_create(c: &mut Criterion) {
-  c.bench_function("bufdata_create", move |b| {
+  c.bench_function("bench_bufdata_create", move |b| {
     let origlines = Lines::read_file("files/example.pc").expect("1");
     let lines = Lines::from_slice(&origlines);
     let keywords = Keywords::from_lines(&lines);
@@ -19,8 +102,8 @@ fn bench_bufdata_create(c: &mut Criterion) {
   });
 }
 
-fn bench_bufdata_change_line(c: &mut Criterion) {
-  c.bench_function("bufdata_change_line", move |b| {
+fn bench_bufdata_readonly(c: &mut Criterion) {
+  c.bench_function("bench_bufdata_readonly", move |b| {
     let origlines = Lines::read_file("files/example.pc").expect("1");
     let lines = Lines::from_slice(&origlines);
     let keywords = Keywords::from_lines(&lines);
@@ -28,93 +111,71 @@ fn bench_bufdata_change_line(c: &mut Criterion) {
     bufdata.recreate_all(&keywords, &lines).expect("2");
 
     b.iter(|| {
-      let mut tmp_bufdata = black_box(BufData::new());
-      tmp_bufdata
-        .recreate_all(&keywords[28..29], &lines[28..29])
-        .expect("3");
-      bufdata.splice(tmp_bufdata, 28, 29, 0);
+      let _calls = black_box(fake_highlight_region(&bufdata, 1000, 10000));
     })
   });
 }
 
-fn bench_bufdata_add_line(c: &mut Criterion) {
-  c.bench_function("bufdata_add_line", move |b| {
-    let origlines = Lines::read_file("files/example.pc").expect("1");
-    let lines = Lines::from_slice(&origlines);
-    let keywords = Keywords::from_lines(&lines);
-    let mut bufdata = BufData::new();
-    bufdata.recreate_all(&keywords, &lines).expect("2");
+hl_bench!(
+  bench_bufdata_change_line_start;
+  lines: (28, 29);
+  spliceto: (28, 29, 0)
+  );
 
-    b.iter(|| {
-      let mut tmp_bufdata = black_box(BufData::new());
-      tmp_bufdata
-        .recreate_all(&keywords[28..30], &lines[28..30])
-        .expect("3");
-      bufdata.splice(tmp_bufdata, 28, 29, 1);
-    })
-  });
-}
+hl_bench!(
+  bench_bufdata_change_line_end;
+  lines: (20500, 20501);
+  spliceto: (20500, 20501, 0)
+  );
 
-fn bench_bufdata_delete_line(c: &mut Criterion) {
-  c.bench_function("bufdata_delete_line", move |b| {
-    let origlines = Lines::read_file("files/example.pc").expect("1");
-    let lines = Lines::from_slice(&origlines);
-    let keywords = Keywords::from_lines(&lines);
-    let mut bufdata = BufData::new();
-    bufdata.recreate_all(&keywords, &lines).expect("2");
+hl_bench!(
+  bench_bufdata_add_line_start;
+  lines: (28, 30);
+  spliceto: (28, 29, 1)
+  );
 
-    b.iter(|| {
-      let tmp_bufdata = black_box(BufData::new());
-      bufdata.splice(tmp_bufdata, 28, 29, -1);
-    })
-  });
-}
+hl_bench!(
+  bench_bufdata_add_line_end;
+  lines: (20500, 20502);
+  spliceto: (20500, 20501, 1)
+  );
 
-fn bench_bufdata_paste_after(c: &mut Criterion) {
-  c.bench_function("bufdata_paste_after", move |b| {
-    let origlines = Lines::read_file("files/example.pc").expect("1");
-    let lines = Lines::from_slice(&origlines);
-    let keywords = Keywords::from_lines(&lines);
-    let mut bufdata = BufData::new();
-    bufdata.recreate_all(&keywords, &lines).expect("2");
+hl_bench!(
+  bench_bufdata_delete_line_start;
+  lines: (0, 0);
+  spliceto: (28, 29, -1i64)
+  );
 
-    let last = lines.len() - 1;
+hl_bench!(
+  bench_bufdata_delete_line_end;
+  lines: (0, 0);
+  spliceto: (20500, 20501, -1i64)
+  );
 
-    b.iter(|| {
-      let mut tmp_bufdata = black_box(BufData::new());
-      tmp_bufdata
-        .recreate_all(&keywords[28..35], &lines[28..35])
-        .expect("3");
-      bufdata.splice(tmp_bufdata, last, last, 7);
-    })
-  });
-}
+hl_bench!(
+  bench_bufdata_paste_after;
+  lines: (28, 35);
+  spliceto: (20586, 20586, 7)
+  );
 
-fn bench_bufdata_paste_before(c: &mut Criterion) {
-  c.bench_function("bufdata_paste_before", move |b| {
-    let origlines = Lines::read_file("files/example.pc").expect("1");
-    let lines = Lines::from_slice(&origlines);
-    let keywords = Keywords::from_lines(&lines);
-    let mut bufdata = BufData::new();
-    bufdata.recreate_all(&keywords, &lines).expect("2");
+hl_bench!(
+  bench_bufdata_paste_before;
+  lines: (28, 35);
+  spliceto: (0, 0, 7)
+  );
 
-    b.iter(|| {
-      let mut tmp_bufdata = black_box(BufData::new());
-      tmp_bufdata
-        .recreate_all(&keywords[28..35], &lines[28..35])
-        .expect("3");
-      bufdata.splice(tmp_bufdata, 0, 0, 7);
-    })
-  });
-}
 criterion_group!(
   name = hl_splice;
   config = Criterion::default().sample_size(10).without_plots();
-  targets = bench_bufdata_change_line,
-            bench_bufdata_create,
-            bench_bufdata_add_line,
-            bench_bufdata_delete_line,
+  targets = bench_bufdata_create,
+            bench_bufdata_change_line_start,
+            bench_bufdata_change_line_end,
+            bench_bufdata_add_line_start,
+            bench_bufdata_add_line_end,
+            bench_bufdata_delete_line_start,
+            bench_bufdata_delete_line_end,
             bench_bufdata_paste_after,
-            bench_bufdata_paste_before
+            bench_bufdata_paste_before,
+            bench_bufdata_readonly,
 );
 criterion_main!(hl_splice);
